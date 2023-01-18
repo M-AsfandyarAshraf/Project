55f9a47d50debd269bb86d0566b97eca4d7a74c3
Muhammad Asfandyar Ashraf <AshraAsf@corp.contoursoftware.net>
2023-01-18 17:10:44 +0500

    email validation for contact form

--- contact.js
+++ contact.js
@@ -13,18 +13,25 @@
 var validate = () => {
 
 
-    var email = document.getElementById("email").value;
+    var email = document.getElementById("email");
     var name = document.getElementById("name");
     var project = document.getElementById("project");
     var subject = document.getElementById("subject");
-    
-    if((!email.innerHTML   includes("@") && !email.includes(".")) || empty)
-    {
-        email.style.backgroundColor = "red";
-        email.style.borderBlockColor = "red";
-        alert("00");
-        return true;
 
+    var mailformat = /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/;
+
+    
+    if(email.value.match(mailformat) || name.value==="" || name.value==="" || subject.value==="" )
+    {
+                
+        return false;
+        
+
+    }
+    else{
+        email.style.borderColor ="Red"
+        document.getElementById("email_error").style.display = "block";
+        return true;
     }
     
 }
