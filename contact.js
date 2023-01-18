c7410f1f5fd49b92c95fb4126ef26debc9b19c2c
Muhammad Asfandyar Ashraf <AshraAsf@corp.contoursoftware.net>
2023-01-18 18:24:35 +0500

    completed form validation for contact.html

--- contact.js
+++ contact.js
@@ -1,37 +1,71 @@
 
 
-    
-    // alert("helllo");
-    document.addEventListener("submit", function(event){
-        if(validate()) {
-            event.preventDefault();
-        }
+
+// alert("helllo");
+document.addEventListener("submit", function (event) {
+    if (validate()) {
+        event.preventDefault();
+    }
 
 
-    });
+});
 
 var validate = () => {
 
 
+    var mailformat = /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/;
+
     var email = document.getElementById("email");
-    var name = document.getElementById("name");
+    var fname = document.getElementById("name");
     var project = document.getElementById("project");
     var subject = document.getElementById("subject");
 
-    var mailformat = /^[_A-Za-z0-9-\+]+(\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9]+)*(\.[A-Za-z]{2,})$/;
+    if (email.value.match(mailformat) && (fname.value.length > 0) && (project.value.length > 0) && (subject.value.length > 0)) {
 
-    
-    if(email.value.match(mailformat) || name.value==="" || name.value==="" || subject.value==="" )
-    {
-                
+      
         return false;
         
-
     }
-    else{
-        email.style.borderColor ="Red"
-        document.getElementById("email_error").style.display = "block";
+
+
+    else {
+        if (!email.value.match(mailformat)) {
+            email.style.borderColor = "Red";
+            document.getElementById("email_error").style.display = "block";
+        }
+        if (fname.value.length == 0) {
+            fname.style.borderColor = "Red";
+            document.getElementById("name_error").style.display = "block";
+
+        }
+        if (project.value.length == 0) {
+            project.style.borderColor = "Red";
+            document.getElementById("project_error").style.display = "block";
+
+        }
+        if (subject.value.length == 0) {
+            subject.style.borderColor = "Red";
+            document.getElementById("subject_error").style.display = "block";
+
+        }
         return true;
     }
-    
+
 }
+
+
+
+document.addEventListener("keyup", function (event) {
+
+
+    document.getElementById("email_error").style.display = "none";
+    document.getElementById("name_error").style.display = "none";
+    document.getElementById("project_error").style.display = "none";
+    document.getElementById("subject_error").style.display = "none";
+    document.getElementById("subject").style.borderColor = "Grey";
+    document.getElementById("email").style.borderColor = "Grey";
+    document.getElementById("name").style.borderColor = "Grey";
+    document.getElementById("project").style.borderColor = "Grey";
+    
+
+});
