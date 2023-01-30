59c2cf5ba183891ce5f87607af72b8b763c9c6a2
Muhammad Asfandyar Ashraf <AshraAsf@corp.contoursoftware.net>
2023-01-30 18:57:39 +0500

    completion of matrix multiplication from quiz 2 and counter button

--- quiz2.js
+++ quiz2.js
@@ -100,17 +100,17 @@ var AgeCalculator = () => {
     var today = Date.now();
 
 
-    if(today > dob) {
+    if (today > dob) {
 
-        var diffday = Math.floor((today-dob)/(1000*60*60*24));
-        
-        var years  = Math.floor(diffday/365);
-        var months = Math.floor((diffday%365)/31);
-        var days = Math.floor((diffday%365)%31);
+        var diffday = Math.floor((today - dob) / (1000 * 60 * 60 * 24));
 
-        alert("Your age is : "+ years + " Years,  "+months + " Months, " + days + "Days");
+        var years = Math.floor(diffday / 365);
+        var months = Math.floor((diffday % 365) / 31);
+        var days = Math.floor((diffday % 365) % 31);
+
+        alert("Your age is : " + years + " Years,  " + months + " Months, " + days + "Days");
     }
-    
+
 }
 
 
@@ -120,22 +120,89 @@ var AgeCalculator = () => {
 // Section IV q1
 
 
-var matrixMultiply = () => {
+var matrixValidate = () => {
     var rowA = document.getElementById("rowA").value;
     var colA = document.getElementById("colA").value;
     var rowB = document.getElementById("rowB").value;
     var colB = document.getElementById("colB").value;
-    
 
-    if(colA !== rowB)
-    {
+
+    if (colA !== rowB) {
         alert("Incompatible matrices");
     }
-    else
-    {
+    else {
         document.getElementById("dimension").style.display = "none";
         document.getElementById("matrix").style.display = "block";
+        document.getElementById("matrixBtn").addEventListener("click", () => { matrixMultiply(rowA, rowB, colA, colB) });
 
     }
 
+}
+
+
+var matrixMultiply = (rowA, rowB, colA, colB) => {
+
+    // var arr = new Array();
+    var arrA = new Array();
+    var arrB = new Array();
+    var matA = new Array();
+    var matB = new Array();
+    var matC = new Array();
+    var result = new Array();
+    var count;
+    var a = document.getElementById("inputA").value;
+    var b = document.getElementById("inputB").value;
+
+    arrA = a.split(',');
+    arrB = b.split(',');
+    arrA = arrA.map(Number);
+    arrB = arrB.map(Number);
+
+
+    try {
+
+        if (arrA.length !== (rowA * colA) || arrB.length !== (rowB * colB)) {
+            var err = "Input doesnt match the dimensions provided earlier!";
+            throw err;
+
+        }
+
+    }
+    catch (err) {
+        alert(err);
+    }
+
+    while(arrA.length) matA.push(arrA.splice(0,colA));
+    while(arrB.length) matB.push(arrB.splice(0,colB));
+
+ 
+
+    for (var i=0; i<rowA; i++)
+    {
+
+        for(var j=0; j<colB; j++)
+        {
+            count=0;
+            for(var k=0; k<colA; k++)
+            {
+                count += (matA[i][k] * matB[k][j]);
+                  
+            } 
+            matC.push(count);
+
+        }
+    }
+    
+    while(matC.length) result.push(matC.splice(0,colA));
+    alert("The resultant matrix is "+result);
+    console.log(result);
+
+
+}
+
+
+var counter =() => {
+    var c = Number(document.getElementById("counter").innerHTML);
+    c += 1;
+    document.getElementById("counter").innerHTML = c;
 }
